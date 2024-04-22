227aaad29a62109aafefa9b48b2ebfce50e31b28
RomainP01 <romain.pani@decathlon.com>
2024-04-22 15:15:28 +0200

    feat(tests) more tests

--- bff/src/test/java/com/masterproject/musigame/rooms/RoomsServiceTests.java
+++ bff/src/test/java/com/masterproject/musigame/rooms/RoomsServiceTests.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import static com.masterproject.musigame.rooms.RoomMother.Rooms.ids;
+import static com.masterproject.musigame.rooms.RoomMother.generateCreator;
+import static com.masterproject.musigame.rooms.RoomMother.roomBuilder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("unit")
@@ -23,10 +26,30 @@ class RoomsServiceTests {
     @Test
     @DisplayName("create a room successfully")
     void createRoom() {
-        Room room = RoomMother.roomBuilder().buildNoPlayers();
+        Room room = roomBuilder().buildNoPlayers();
         var actual = service.save(room.getCreator());
 
         assertThat(actual).usingRecursiveComparison().ignoringFields("roomId.value").isEqualTo(room);
+    }
+
+    @Test
+    @DisplayName("get a room with known RoomId")
+    void getRoomWithKnownRoomId() {
+        Creator creator = generateCreator();
+        var expected = service.save(creator);
+
+        var actual = service.findById(expected.getRoomId());
+        assertThat(actual.get()).isEqualTo(expected);
+    }
+
+    @Test
+    @DisplayName("get null with unknown RoomId")
+    void getNullWithUnknownRoomId() {
+        Creator creator = generateCreator();
+        service.save(creator);
+
+        var actual = service.findById(ids().sample());
+        assertThat(actual.isEmpty());
     }
 
 
