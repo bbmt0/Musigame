fdf5188a3ac85354e99ca454d9f84bf89ae7e472
bbmt0 <benoit.beaumont@protonmail.com>
2024-05-11 21:21:07 +0200

    sharelink page creation

---
+++ front/src/pages/sharelinkscreen/ShareLinkScreenStyles.js
@@ -0,0 +1,15 @@
+import styles from '../../assets/styles/styles';
+
+const ShareLinkScreenStyles = {
+    container: {
+        ...styles.container,
+    },
+    backButton: {
+        ...styles.backButton,
+    },
+    h4: {
+        ...styles.h4,
+        marginTop: '30vh'
+    },
+};
+export default ShareLinkScreenStyles;
