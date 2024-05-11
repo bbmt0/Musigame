8bfea525e0db6f5e69b190588bd60cc00d441d76
bbmt0 <benoit.beaumont@protonmail.com>
2024-05-11 21:24:57 +0200

    game screens creations

---
+++ front/src/pages/gamescreens/playerscreens/PlayerScreenStyles.js
@@ -0,0 +1,35 @@
+import styles from '../../../assets/styles/styles';
+import colors from '../../../assets/styles/colors';
+
+
+export const PlayerScreenStyles = {
+    container: {
+        ...styles.container,
+    },
+    backButton: {
+        ...styles.backButton,
+    },
+    h4: {
+        ...styles.h4,
+        marginTop: '10vh'
+    },
+    smallText: {...styles.smallText, 
+        marginTop: '2vh',
+        marginLeft: '6vw'
+    }, 
+    gif: {
+        marginTop: '5vh'
+    },
+    situationText : {
+        margin: 0,
+        marginBottom: '1vh',
+        fontSize: '1.5em',
+        color: colors.MG_TEAL,
+    }, 
+    confirmationBox: {
+        display: 'flex', 
+        justifyContent: 'space-between', 
+        width: '70vw',
+    }
+
+};
