366d16ff5294c67dfbd2d1d8f430e89e71152e85
bbmt0 <benoit.beaumont@protonmail.com>
2024-05-12 21:47:50 +0200

    adding assets folder

---
+++ front/src/assets/styles/styles.js
@@ -0,0 +1,40 @@
+import backgroundImage from '../images/background.png';
+import colors from './colors';
+const styles = {
+    h1: {
+        color: colors.MG_GREEN,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    h2: {},
+    h3: {
+        color: 'white',
+        maxWidth: '50%'
+    },
+    h4: {
+        width: '85vw',
+        color: colors.MG_TEAL,
+        textTransform: 'uppercase',
+        justifyContent: 'left',
+
+    },
+    smallText: {
+        color: 'white',
+        fontSize: '1em',
+        marginTop: '2em'
+    },
+    container: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: 'cover',
+        height: '100vh',
+    },
+    links: {
+        color: 'white', 
+        textDecoration: 'underline'    
+    }
+}
+
+export default styles;
