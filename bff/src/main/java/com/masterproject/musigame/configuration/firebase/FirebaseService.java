98bb937ed652c518b41d3c0323cde253a653b549
bbmt0 <benoit.beaumont@protonmail.com>
2024-07-09 16:35:26 +0200

    update bff firebase key with variable env in Azure

--- bff/src/main/java/com/masterproject/musigame/configuration/firebase/FirebaseService.java
+++ bff/src/main/java/com/masterproject/musigame/configuration/firebase/FirebaseService.java
@@ -5,22 +5,28 @@ import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.cloud.FirestoreClient;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
 
 @Service
 public class FirebaseService {
     Firestore db;
 
     public FirebaseService() throws IOException {
-        File file = new File(
-                getClass().getClassLoader().getResource("configuration/firebase_key.json").getFile()
-        );
-        FileInputStream fis = new FileInputStream(file);
-        GoogleCredentials credentials = GoogleCredentials.fromStream(fis);
+        InputStream serviceAccount;
+
+        try {
+            serviceAccount = new ClassPathResource("configuration/firebase_key.json").getInputStream();
+        } catch (IOException e) {
+            String firebaseKey = System.getenv("FIREBASE_KEY");
+            if (firebaseKey == null) {
+                throw new IOException("Firebase key not found in environment variables or resources");
+            }
+            serviceAccount = new ByteArrayInputStream(firebaseKey.getBytes());
+        }
+        GoogleCredentials credentials = GoogleCredentials.fromStream(serviceAccount);
         FirebaseOptions options = new FirebaseOptions.Builder()
                 .setCredentials(credentials)
                 .build();
