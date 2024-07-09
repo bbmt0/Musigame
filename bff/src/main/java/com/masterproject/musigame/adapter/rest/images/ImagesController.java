3277552bedbb1c159d57519eb8ba263ed2f9196d
bbmt0 <benoit.beaumont@protonmail.com>
2024-07-09 11:08:47 +0200

    bff allow cross origins images endpoint

--- bff/src/main/java/com/masterproject/musigame/adapter/rest/images/ImagesController.java
+++ bff/src/main/java/com/masterproject/musigame/adapter/rest/images/ImagesController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
-@CrossOrigin("http://www.musigame-front.azurewebsites.net")
+@CrossOrigin("*")
 @Slf4j
 @RestController
 @RequiredArgsConstructor
